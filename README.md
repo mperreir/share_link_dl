12742e20a926efb96e1da91a8ceed3451ead4588
Matthieu Perreira Da Silva <matthieu.perreiradasilva@univ-nantes.fr>
2020-10-19 12:04:34 +0200

    First version of the script
    Far from being bullet proof... but at leat is seems to work as intended and is very simple (no additionnal module to install)

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # share_link_dl
-Python 3 script for downloading the content of a seafile folder using a (public) share link
+Python 3 script for downloading the content of a Seafile folder using a (public) share link
+
+## Why should I use this script ?
+
+Seafile share links are great, but if you want to download a folder Seafile will have to generate a zip file. This is not a limitation of Seafile, all other cloud storage web client do the same as it is not possible for a web browser to create folders. If the shared folder is big, Seafile might take a very long time generating the zip archive of will simply refuse to create it. In that case the only thing you can do is downloading each file manually :-(.
+
+This script helps solving this problem by automating the download of a shared folder without any zip file creation :-).
+
+## Requirements
+
+This script uses only standard python 3 modules, so you only need python 3 on your system.
+
+## Usage
+
+ `share_link_dl.py share_link_url [destination_path]`
+
+where:
+
+- share_link_url : url created using the "create share link" feature of Seafile. It usually look like http://url_of_seafile_server/d/long_random_string
+- destination_path : is the path where you want to store the content of the shared folder on you local filesystem. If this parameter is omitted the default location is the current folder `.`  (no enclosing folder is created by default so be careful when downloading a folder containing lots of files at its root)
---
+++ share_link_dl.py
@@ -0,0 +1,69 @@
+import urllib.request, urllib.parse
+import sys, os, math, json
+import os.path
+
+# Formats a size in bytes into a human readable string
+def format_size(num, suffix='B'):
+    if num > 0:
+        magnitude = int(math.floor(math.log(num, 1024)))
+    else:
+        magnitude = 0
+    val = num / math.pow(1024, magnitude)
+    if magnitude > 7:
+        return '{:.1f}{}{}'.format(val, 'Y', suffix)
+    return '{:3.1f}{}{}'.format(val, ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z'][magnitude], suffix)
+
+# Simple utility function for displaying a text based progress bar
+def report_progress( transfered, block_size, total_size, progress_size = 40):
+    if block_size < total_size:
+        current_size = transfered*block_size
+    else:
+        current_size = total_size
+    progress = progress_size*current_size//total_size
+    print('\r', end='')
+    for i in range(progress):
+        print("*", end='')
+    for i in range(progress,progress_size):
+        print(" ", end='')
+    print("\t{}/{}   ".format(format_size(current_size), format_size(total_size)), end='')
+
+# Queriies seafile API and recursively downloads all the files in the folder
+# shared by a share link
+def process_dir_url( base_url, path, destination_path ):
+    opener = urllib.request.build_opener()
+    url = base_url + '?path=' + urllib.parse.quote(path)
+    try:
+        with opener.open(url) as r:
+            data = r.read().decode()
+            json_data = json.loads(data)
+            for entry in json_data['dirent_list']:
+                if entry['is_dir'] == True:
+                    process_dir_url( base_url, entry['folder_path'], destination_path)
+                else:
+                    file_url = "{}/files/?p={}&dl=1".format(original_url,urllib.parse.quote(entry['file_path']))
+                    complete_dst_path = os.path.join(destination_path, os.path.split(entry['file_path'][1:])[0])
+                    if not os.path.exists(complete_dst_path):
+                        os.makedirs(complete_dst_path)
+                    print("downloading:", entry['file_path'][1:])
+                    file_path = os.path.join(destination_path, urllib.parse.quote(entry['file_path'][1:]))
+                    urllib.request.urlretrieve(file_url, file_path, report_progress)
+                    print()
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__":
+    try:
+        destination_path = '.'
+        if len(sys.argv) == 1:
+            print("Usage : share_link_dl.py share_link_url [destination_path]")
+            exit()
+        elif len(sys.argv) == 3:
+            destination_path = sys.argv[2]
+            if not os.path.exists(destination_path):
+                os.makedirs(destination_path)
+
+        original_url = sys.argv[1]
+        base_url = original_url.replace('/d/', '/api/v2.1/share-links/') + '/dirents/'
+        process_dir_url(base_url, '/', destination_path)
+    except KeyboardInterrupt as e:
+        print("\nDownload interrupted by user (Ctrl+C)")
